62a8cf6e248d4561d654453355f8862ba58a4cdb
Pedro Dias <pedrodiasfonseca@gmail.com>
2014-07-16 17:05:56 -0300

    Adiciona informações dos políticos na Homepage dinamicamente

---
+++ app/scripts/controllers/homepage.js
@@ -0,0 +1,15 @@
+'use strict'
+
+angular.module('projetobrasilFrontApp')
+  .controller('HomepageCtrl', function($scope, $http) {
+
+    $http.get('http://api.projetobrasil.org:4242/v1/politicians').
+      success(function(data, status, headers, config) {
+        $scope.politicos = data;
+      }).
+      error(function(data, status, headers, config) {
+        alert(status);
+      });
+
+
+  });
