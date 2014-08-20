b6da5b8557b7a57e9e6334e18929c2d80869e459
Pedro Dias <pedrodiasfonseca@gmail.com>
2014-08-19 21:06:50 -0300

    Adiciona informações dinâmicas ao ranking de politicos usando o serviço do teste cego

--- app/scripts/politicians-ranking/politicians-ranking.js
+++ app/scripts/politicians-ranking/politicians-ranking.js
@@ -1,16 +1,44 @@
 'use strict'
 
 angular.module('projetobrasilFrontApp')
-  .controller('PoliticiansRankingCtrl', ['$scope', 'UserRatings', 'profileGetter', function($scope, UserRatings, profileGetter){
+  .controller('PoliticiansRankingCtrl', ['$scope', '$rootScope', 'UserRatings', 'profileGetter', 'testeCego',
+    function($scope, $rootScope, UserRatings, profileGetter, testeCego){
 
     $scope.position = 0;
 
     profileGetter.getProfile().then(function(profiles) {
-        if(!profiles) return;
-        $scope.politicians = profiles;
+      if(!profiles) return;
+      $scope.politicians = profiles;
+    });
+
+    $scope.ranking = {};
+    $scope.ranking.num_total_propostas = 0;
+    $scope.ranking.somatorio_notas = 0;
+    testeCego.propostasAvaliadas.query(function(propostas){
+      var propostas_agrupadas = _.groupBy(propostas, function(prop){
+        $scope.ranking.somatorio_notas += prop.nota;
+        $scope.ranking.num_total_propostas += 1;
+        return prop.politicians_id
       });
 
-  }])
+
+      _.each(propostas_agrupadas, function(grupo, id){
+
+        $scope.ranking[id] = {}
+
+        var soma_notas = _.reduce(grupo, function(memo, proposal){
+          return memo + proposal.nota
+        }, 0);
+
+        $scope.ranking[id].soma = soma_notas;
+        $scope.ranking[id].num_propostas = grupo.length;
+
+      });
+
+    });
+
+    }
+  ])
   .directive('politiciansRanking', [function(){
     function link(scope, element, attrs){}
 
--- app/scripts/teste-cego/teste-cego.ctrl.js
+++ app/scripts/teste-cego/teste-cego.ctrl.js
@@ -1,48 +1,48 @@
 'use strict';
 
 angular.module('projetobrasilFrontApp')
-.controller('TesteCegoCtrl',
-  function ($scope, $rootScope, proposalsGetter, hotkeys, profileGetter, $filter) {
+  .controller('TesteCegoCtrl', [ '$scope', '$rootScope', 'proposalsGetter', 'hotkeys', 'profileGetter', '$filter',
+    function ($scope, $rootScope, proposalsGetter, hotkeys, profileGetter, $filter) {
 
-    $scope.proposals = {};
-    var proposalIndex = 0;
+      $scope.proposals = {};
+      var proposalIndex = 0;
 
-    profileGetter.getProfile().then(function(profiles) {
-      if(!profiles) return;
-      // $scope.userVotes = UserRatings.get();
-      // profiles = $filter('orderBy')(profiles, 'nome_urna');
-      $scope.profiles = profiles;
-      // $scope.$parent.setActiveByName(profileName);
+      profileGetter.getProfile().then(function(profiles) {
+        if(!profiles) return;
+        // $scope.userVotes = UserRatings.get();
+        // profiles = $filter('orderBy')(profiles, 'nome_urna');
+        $scope.profiles = profiles;
+        // $scope.$parent.setActiveByName(profileName);
 
-    });
+      });
 
-    hotkeys.bindTo($scope)
-    .add({
-      combo: '1',
-      description: 'Vota 1',
-      callback: function(){
-        $rootScope.$broadcast('rate', 1);
-      }
-    })
+      hotkeys.bindTo($scope)
+      .add({
+        combo: '1',
+        description: 'Vota 1',
+        callback: function(){
+          $rootScope.$broadcast('rate', 1);
+        }
+      })
 
-    $scope.getSomeRandomProposals = function(){
-      proposalsGetter.getRandomProposals(150).then(
-        function(proposalsList){
-          $scope.proposals = proposalsList;
-          $scope.getNextProposal();
-          proposalIndex = 1;
-        });
-    };
+      $scope.getSomeRandomProposals = function(){
+        proposalsGetter.getRandomProposals(150).then(
+          function(proposalsList){
+            $scope.proposals = proposalsList;
+            $scope.getNextProposal();
+            proposalIndex = 1;
+          });
+      };
 
-    $scope.getNextProposal = function(){
-      $scope.proposal = $scope.proposals[proposalIndex++];
-    };
-
-
-    // $scope.$on('successfulRating', function(){
-    //   alert('oi');
-    // });
+      $scope.getNextProposal = function(){
+        $scope.proposal = $scope.proposals[proposalIndex++];
+      };
 
 
+      // $scope.$on('successfulRating', function(){
+      //   alert('oi');
+      // });
 
-  });
+
+
+  }]);
