dd8d67f7b9766002b499c8b12505a6140f49a5b5
Pedro Dias <pedrodiasfonseca@gmail.com>
2014-08-27 17:38:22 -0300

    Coloca uma chamada do html.Readdy() na Homepage

--- app/scripts/controllers/homepage.js
+++ app/scripts/controllers/homepage.js
@@ -6,6 +6,19 @@ angular.module('projetobrasilFrontApp')
     profileGetter.getProfile().then(function(profiles) {
         if(!profiles) return;
         $scope.politicos = profiles;
+
+        $scope.pageTitle = "Projeto Brasil";
+        $scope.og = {
+          url : $location.absUrl(),
+          siteName : 'projetobrasil.org',
+          title : 'Projeto Brasil',
+          description : 'Seus próximos 4 anos serão impactados por uma decisão que demora menos de 4 segundos: o voto para presidente! Por isso conheça, compare e avalie propostas e candidatos no Projeto Brasil! A hora é essa!',
+          imageUrl : 'http://www.projetobrasil.org/images/urna.jpg',
+          imageWidth : '640',
+          imageHeight : '304'
+        };
+        $scope.htmlReady();
+
       });
 
   });
